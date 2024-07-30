933a7b822829f22e935a35ce4bf10320217aae42
Ksusha Pistsova <ksushapistsova@gmail.com>
2024-07-31 00:10:48 +0300

    add alarm_clock.kv, box layout in it and class alarm_clock in alarm_clock.py

--- Alarm clock/Alarm_clock.py
+++ Alarm clock/Alarm_clock.py
@@ -1,1 +1,16 @@
+# -*- coding: utf-8 -*-
+import kivy 
+from kivy.app import App
+from kivy.uix.bubble import BoxLayout 
+
+kivy.require('1.9.0')       
+# '2.0.0.' стоит попробовать потом, зависит от телефона
+
+class Alarm_clock(App):
+    def build(self):
+        return BoxLayout()
+    
+neuralRandom = Alarm_clock()
+neuralRandom.run()
+        
 
